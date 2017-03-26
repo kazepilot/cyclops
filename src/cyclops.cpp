e1ab27fc7f06c8e6ffd76bfbda89b5b2458be04e
Evaldas <evaldas@evalkaz.com>
2017-03-26 15:02:37 +0300

    Added image decodation

--- src/cyclops.cpp
+++ src/cyclops.cpp
@@ -1,15 +1,23 @@
-#include "crow_all.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-#include <sstream>
+#include "crow_all.h"
 
 int main()
 {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")
-        .name("hello")
-    ([]{
-        return "Hello World!";
+    CROW_ROUTE(app, "/cyclops")
+    .methods("POST"_method)
+    ([&](const crow::request& req)
+    {
+        std::vector<unsigned char> encoded(req.body.begin(), req.body.end());
+
+        cv::Mat image = cv::imdecode(cv::Mat(encoded), 1);
+
+        return "Image decoded";
     });
 
     app.port(1234)
